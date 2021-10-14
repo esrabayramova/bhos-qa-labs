e278ab5d148c11e771bca63c9372a490cccd90a1
esrabayramova <esra.bayramova24@gmail.com>
2021-10-14 22:52:31 +0400

    updated

--- src/test/java/TestCuboid.java
+++ src/test/java/TestCuboid.java
@@ -11,4 +11,15 @@ public class TestCuboid {
     void test_surface_area(){
         assertEquals(232, Cuboid.surface_area(4, 7, 8));
     }
+
+    Cuboid new_cuboid = new Cuboid(2, 3, 4);
+
+    @Test
+    void test_height() { assertEquals(2, new_cuboid.getHeight());}
+
+    @Test
+    void test_length() { assertEquals(3, new_cuboid.getLength());}
+
+    @Test
+    void test_width() { assertEquals(4, new_cuboid.getWidth());}
 }
