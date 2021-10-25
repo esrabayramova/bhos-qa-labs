3fe970323c2f1d5f51d4ba30e8a5c38f6a0eee84
esrabayramova <esra.bayramova24@gmail.com>
2021-10-25 21:30:00 +0400

    added

---
+++ src/main/java/com/example/springbproj2/Springbproj2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbproj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbproj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbproj2Application.class, args);
+	}
+
+}
