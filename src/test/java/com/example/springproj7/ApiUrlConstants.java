9379db1c618b707a4468142847b2bd76d18463c1
esrabayramova <esra.bayramova24@gmail.com>
2021-12-03 09:27:33 +0400

    Added

---
+++ src/test/java/com/example/springproj7/ApiUrlConstants.java
@@ -0,0 +1,9 @@
+package com.example.springproj7;
+
+public class ApiUrlConstants {
+
+    public final static String FIREBASE_SIGNIN_EMAIL_AND_PASSWORD = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
+    public final static String FIRESTORE_DOCUMENT = "https://firestore.googleapis.com/v1/projects/spring-boot-proj-255f3/databases/(default)/documents/users";
+    public final static String FIREBASE_STORAGE_AVATAR = "https://firebasestorage.googleapis.com/v0/b/spring-boot-proj-255f3.appspot.com/o";
+
+}
