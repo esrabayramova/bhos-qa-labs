8a3f55f322a18d984e3dea981afa4ad158a79734
esrabayramova <esra.bayramova24@gmail.com>
2021-12-20 01:24:24 +0400

    added

---
+++ src/test/java/com/example/springproject8/Springproject8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproject8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproject8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
